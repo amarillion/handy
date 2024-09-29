de4ce7e42f7ddf6a038824c9059c7ea9128bb06e
Martijn van Iersel <mvaniersel@gmail.com>
2024-09-29 13:08:20 +0200

    First app: QR creator

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import QrCreator from './components/QrCreator.vue';
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <QrCreator/>
 </template>
 
 <style scoped>
